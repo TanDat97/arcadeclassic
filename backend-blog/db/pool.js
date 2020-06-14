e1d7b004ff32bf3bf826a7bf873275bd53fdec2f
Tran Tan Dat <trantandat130497@gmail.com>
2020-06-14 18:35:25 +0700

    add dockerfile for nodejs and postgres

--- backend-blog/db/pool.js
+++ backend-blog/db/pool.js
@@ -4,7 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  // connectionString: process.env.DATABASE_URL
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
 });
 
 module.exports = pool
--- studio/src/utils/UrlUtils.js
+++ studio/src/utils/UrlUtils.js
@@ -1,13 +1,13 @@
 export default {
   api: {
-    userSignin: 'http://localhost:5000/api/user/signin',
-    userInfo: 'http://localhost:5000/api/user',
+    userSignin: 'http://localhost:8080/api/user/signin',
+    userInfo: 'http://localhost:8080/api/user',
     
   },
   admin: {
-    getListPostFilter: 'http://localhost:5000/api/admin/post/get_list/filter',
-    getOnePost: 'http://localhost:5000/api/admin/post/get_one',
-    getCategoryRoot: 'http://localhost:5000/api/admin/category/get_list_root',
-    getCategoryChild: 'http://localhost:5000/api/admin/category/get_list_child'
+    getListPostFilter: 'http://localhost:8080/api/admin/post/get_list/filter',
+    getOnePost: 'http://localhost:8080/api/admin/post/get_one',
+    getCategoryRoot: 'http://localhost:8080/api/admin/category/get_list_root',
+    getCategoryChild: 'http://localhost:8080/api/admin/category/get_list_child'
   }
 }
