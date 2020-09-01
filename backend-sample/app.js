83e04398b1fc472f54e747b7851a2e490fd32fe8
Tran Tan Dat <trantandat130497@gmail.com>
2020-09-01 22:17:14 +0700

    change

--- backend-sample/app.js
+++ backend-sample/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sessions = require('express-session');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 
@@ -15,6 +15,7 @@ const corsOption = {
   credentials: true,
   exposedHeaders: ['authorization']
 }
+
 app.use(cors(corsOption))
 
 app.use(sessions({  
@@ -35,25 +36,24 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-
 const indexRouter = require('./routes/index')
 
 app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404))
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error');
-});
+})
 
-module.exports = app;
+module.exports = app
